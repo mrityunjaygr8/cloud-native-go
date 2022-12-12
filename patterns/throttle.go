842fc7047ff64a192220caed2c3cac9a92212b04
Mrityunjay Saxena <14573967+mrityunjaygr8@users.noreply.github.com>
2022-12-12 19:34:32 +0530

    throttle pattern added

---
+++ patterns/throttle.go
@@ -0,0 +1,48 @@
+package patterns
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
+
+func Throttle(e Effector, max uint, refill uint, d time.Duration) Effector {
+	var tokens = max
+	var once sync.Once
+
+	return func(ctx context.Context) (string, error) {
+		if ctx.Err() != nil {
+			return "", ctx.Err()
+		}
+
+		once.Do(func() {
+			ticker := time.NewTicker(d)
+
+			go func() {
+				defer ticker.Stop()
+
+				for {
+					select {
+					case <-ctx.Done():
+						return
+					case <-ticker.C:
+						t := tokens + refill
+						if t > max {
+							t = max
+						}
+						tokens = t
+					}
+				}
+			}()
+		})
+
+		if tokens <= 0 {
+			return "", fmt.Errorf("too many calls")
+		}
+
+		tokens--
+
+		return e(ctx)
+	}
+}
